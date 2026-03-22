a000301a77de82c6c3b1d3105bfb6b460ba2d9fa
Renan Souza <renaninacio506@gmail.com>
2026-03-22 10:05:38 -0300

    feat(entity): criação da entidade Livro com mapeamento JPA

---
+++ lista.exercicios/src/main/java/trabalho/java/lista/exercicios/entity/Livro.java
@@ -0,0 +1,48 @@
+package trabalho.java.lista.exercicios.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Livro")
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_titulo")
+    private String titulo;
+
+    @Column(name = "nome_autor")
+    private String autor;
+
+    private Integer anoPublicacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
